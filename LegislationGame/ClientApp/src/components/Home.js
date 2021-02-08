3cc1bfe09383c4851bbe87baa95c21a854ab5a5d
Grant VN <g.vannost@gmail.com>
2021-02-07 21:32:21 -0500

    Home.js: Realigned intro card. Added link to WP blog. NavMenu.js: Revised links. Game.pdf: Moved to WP.

--- LegislationGame/ClientApp/src/components/Home.js
+++ LegislationGame/ClientApp/src/components/Home.js
@@ -6,9 +6,9 @@ export class Home extends Component {
   render () {
     return (
         <div className="container">
-            <div className="card col-md-3" style={{ width: "25%", float: "right"}}>
+            <div className="card col-lg-3" style={{ float: "right" }}>
                 <div className="card-header">
-                    <h4 class="card-title">Let's play!</h4>
+                    <h4 className="card-title">Let's play!</h4>
                 </div>
                 <div className="card-body">
                     <ul>
@@ -21,9 +21,11 @@ export class Home extends Component {
                     </ul>
                 </div>
             </div>
-            <div className="col-md-9">
-            <h1>Welcome to <u>Legislation: The Game</u>!</h1>
-                <p>Please note this game is in pre-alpha and provided as is. Changes will be made based on availability and player feedback. Loss of data may occur without notice, though we will try to communicate in advance.</p>
+            <div className="col-lg-9">
+                <h1>Welcome to <u>Legislation: The Game</u>!</h1>
+                <p>Please note this game is in pre-alpha and provided as is. Changes will be made based on availability and player feedback.</p>
+                <p>Loss of data may occur without notice, though we will try to communicate in advance.</p>
+                <p>For more information, please visit <a href="https://www.legislationthegame.com/">the blog</a>.</p>
                 <p>This game has been tested in Chrome, Firefox and Edge. Results not guaraunteed in other browsers. Online interactions are not moderated.</p>
                 <p>Last completed maintenance: N/A </p>
                 <p>Next scheduled maintenance: TBD </p>
