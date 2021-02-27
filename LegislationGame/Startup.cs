681cb16bae0295501122a1ce9638a95eb1efa411
Grant VN <g.vannost@gmail.com>
2021-02-27 16:46:30 -0500

    ApplicationUser.cs: Dummy class for later use in authentication. LegislationGame.csproj: Library updates. Startup.cs: Added HTTPS redirection. Added use of authentication/authorization (Identity on hold).

--- LegislationGame/Startup.cs
+++ LegislationGame/Startup.cs
@@ -44,11 +44,15 @@ namespace com.nordstrands.games.Legislation
                 app.UseExceptionHandler("/Error");
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            //app.UseIdentityServer();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
